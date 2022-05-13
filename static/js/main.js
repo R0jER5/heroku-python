dd91f448e50d3adbad726fe994c0e818c7936999
Jaydevsinh Zankat <75049013+R0jER5@users.noreply.github.com>
2022-05-13 20:23:32 +0530

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,22 @@
+$('#button_start').click(function () {
+        // var form_data = new FormData($('#-file')[0]);
 
+
+
+
+$.ajax({
+            type: 'POST',
+            url: '/predict',
+            data: null,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                $('#result').fadeIn(600);
+                $('#result').text(' Result:  ' + data);
+                console.log('Success!');
+            },
+        });
+    })
